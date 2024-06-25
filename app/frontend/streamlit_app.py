abffc756df2a2902c4486f2b11cb239136cd44e1
Manaf Mukred <manafmukred@gmail.com>
2024-06-25 23:25:31 +0200

    code formatting

--- app/frontend/streamlit_app.py
+++ app/frontend/streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
-URL="http://backend:8000/"
-
+URL = "http://backend:8000/"
 
 
 def handle_userinput(user_question, answer):
@@ -14,19 +13,21 @@ def handle_userinput(user_question, answer):
     # Display user message in chat message container
     with st.chat_message("user", avatar="🔍"):
         st.markdown(user_question)
-    with st.chat_message("Assistant", avatar= "🤓"):
-        st.markdown(answer)    
+    with st.chat_message("Assistant", avatar="🤓"):
+        st.markdown(answer)
 
     # Append the chatbot's response to the chat history
-    st.session_state.chat_history = st.session_state.chat_history + "You: \n" + user_question + "\n "
-
-    st.session_state.chat_history = st.session_state.chat_history + "Chatbot: \n" + answer + "\n"
- 
+    st.session_state.chat_history = st.session_state.chat_history + \
+        "You: \n" + user_question + "\n "
+    st.session_state.chat_history = st.session_state.chat_history + \
+        "Chatbot: \n" + answer + "\n"
     # append previous messages
-    st.session_state.messages.append({"role": "user" ,      "content": user_question})
-    st.session_state.messages.append({"role": "Assistant", "content": answer }      )
+    st.session_state.messages.append(
+        {"role": "user",      "content": user_question})
+    st.session_state.messages.append({"role": "Assistant", "content": answer})
     # Clear the user input
     st.session_state.user_input = ""
+
 
 def main():
 
@@ -46,24 +47,23 @@ def main():
         result = {}
         st.subheader("Your documents")
         uploaded_files = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'",type="pdf",
-              accept_multiple_files=True)
+            "Upload your PDFs here and click on 'Process'", type="pdf",
+            accept_multiple_files=True)
         if uploaded_files:
             if st.button("Process"):
                 with st.spinner("Processing"):
-                    files = [("files", (file.name, file.getvalue(), file.type)) for file in uploaded_files]
-                    requests.post(URL+"upload/", files=files)
-
-
+                    files = [("files", (file.name, file.getvalue(), file.type))
+                             for file in uploaded_files]
+                    requests.post(URL+"upload/", files=files, timeout=15)
 
     if user_question:
         # print(st.session_state.chat_history)
         chat_context = f"chat history: \n {st.session_state.chat_history}"
         full_text = chat_context + user_question
-        reply = requests.post(URL+"ask", json= {"question" :  full_text})
+        reply = requests.post(
+            URL+"ask", json={"question":  full_text}, timeout=15)
         result = reply.json()
         handle_userinput(user_question, result["answer"])
-
 
 
 if __name__ == '__main__':
